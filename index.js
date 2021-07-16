eb12cd28f0b6013ff4b08aa00de18800ca127e27
Samantha Yuhan <sysweettooth1@gmail.com>
2021-07-15 17:50:06 -0700

    add array of questions for user input

--- index.js
+++ index.js
@@ -5,20 +5,46 @@ import * as fs from "fs";
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        title: "project",
-        description: "about",
-        installation: "instructions",
-        usage: "information",
-        contribution: "guidelines",
-        test: "instructions"
+        type: "input",
+        name: "title",
+        message: "What is the project title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the description of this project?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the installation instructions?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is this project used for?"
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "What are the guidelines for contributing to this project?"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What are the instructions for testing this project?"
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
