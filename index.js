a20c612fb415bcd83def636dc0c3ef311c6d7818
Samantha Yuhan <sysweettooth1@gmail.com>
2021-07-15 17:37:09 -0700

    Create an array of questions for user input

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ import inquirer from "inquirer";
 import * as fs from "fs";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        title: "project",
+        description: "about",
+        installation: "instructions",
+        usage: "information",
+        contribution: "guidelines",
+        test: "instructions"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
